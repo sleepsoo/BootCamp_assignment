d89474c9a85ed82a2b583b2c52de43d3c969cf9d
sleepsoo <khs12140149@gmail.com>
2025-04-13 21:15:57 +0900

    Update streamlit_assignment3.py

--- Assignment/streamlit_assignment3.py
+++ Assignment/streamlit_assignment3.py
@@ -16,7 +16,7 @@ st.title("🌞 Prophet Forecast with Preprocessed Sunspot Data")
 # ----------------------------------
 # TODO: 'sunspots_for_prophet.csv' 파일을 불러오고, 'ds' 컬럼을 datetime 형식으로 변환하세요.
 df = pd.read_csv(
-    '/Users/pro/Python/MetaCode/Assignment/data/sunspots_for_prophet.csv')
+    'Assignment/data/sunspots_for_prophet.csv')
 df['ds'] = pd.to_datetime(df['ds'])
 
 st.subheader("📄 데이터 미리보기")
